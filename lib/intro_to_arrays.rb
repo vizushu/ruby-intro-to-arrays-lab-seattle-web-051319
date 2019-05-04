edaa6bb557475e0b409e4bcab926fdd22ec45825
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 01:45:25 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+
+def array_with_two_elements
+  two_element = [1, 2]
+  return two_element
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
